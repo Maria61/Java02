4a4868e70822d5dd5b491aec7344956dc41d0cfa
Maria61 <mariaLi61@outlook.com>
2019-07-30 19:22:40 +0800

    JDBC-数据库操作

---
+++ VacationPractice/src/jdbc_imooc/src/com/imooc/action/GoddessAction.java
@@ -0,0 +1,59 @@
+package com.imooc.action;
+/**
+ * 测试增删查改等方法
+ */
+
+import java.util.Date;
+/**
+ * 增删查改等方法的调用！
+ */
+import java.util.List;
+
+import com.imooc.dao.GoddessDao;
+import com.imooc.model.Goddess;
+
+public class GoddessAction {
+	public static void main(String[] args) throws Exception {
+		GoddessDao g = new GoddessDao();
+		
+		//遍历表格
+//		List<Goddess> gs = g.query();
+//		for(Goddess goddess:gs) {
+//			System.out.println(goddess.getUser_name()+","+goddess.getAge());
+//		}
+//		
+		//测试增加
+//		Goddess g1 = new Goddess();
+//		g1.setUser_name("小红");
+//		g1.setAge(22);
+//		g1.setSex(1);
+//		g1.setBirthday(new Date());
+//		g1.setEmail("xioaxia@imooc.com");
+//		g1.setMobile("12345678900");
+//		g1.setCreate_user("ADMIN");
+//		g1.setUpdate_user("ADMIN");
+//		g1.setIsdel(1);
+//		g.addGoddess(g1);
+		
+		//测试更新功能
+//		Goddess g1 = new Goddess();
+//		g1.setUser_name("小红");
+//		g1.setAge(21);
+//		g1.setSex(1);
+//		g1.setBirthday(new Date());
+//		g1.setEmail("xioaxia@imooc.com");
+//		g1.setMobile("12345699999");
+//		g1.setUpdate_user("ADMIN22");
+//		g1.setIsdel(1);
+//		g1.setId(4);
+//		g.updateGoddess(g1);
+		
+		//测试删除功能
+		g.delGoddess(4);
+		
+		//测试查询单个女神信息功能
+		Goddess g2 = g.get(2);
+		System.out.println(g2.toString());
+		
+	}
+}
---
+++ VacationPractice/src/jdbc_imooc/src/com/imooc/dao/GoddessDao.java
@@ -0,0 +1,117 @@
+package com.imooc.dao;
+/**
+ * 增删查改等一些方法
+ */
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.imooc.db.DBUtil;
+import com.imooc.model.Goddess;
+
+public class GoddessDao {
+	
+	public void addGoddess(Goddess g) throws Exception {
+		Connection conn = DBUtil.getConnection();
+		String sql = ""+"insert into imooc_goddess"+"(user_name,sex,age,birthday,email,mobile,"
+		+"create_user,create_date,update_user,update_date,isdel)"
+				+"values("+
+		"?,?,?,?,?,?,?,current_date(),?,current_date(),?)"; 
+		PreparedStatement ptmt = conn.prepareStatement(sql);
+		
+		ptmt.setString(1, g.getUser_name());
+		ptmt.setInt( 2, g.getSex());
+		ptmt.setInt(3,g.getAge());
+		ptmt.setDate(4,new Date( g.getBirthday().getTime()));
+		ptmt.setString(5, g.getEmail());
+		ptmt.setString(6, g.getMobile());
+		ptmt.setString(7, g.getCreate_user());
+		ptmt.setString(8, g.getUpdate_user());
+		ptmt.setInt(9, g.getIsdel());
+		ptmt.execute(); 
+	}
+	public void delGoddess(Integer id) throws SQLException {
+		Connection conn = DBUtil.getConnection();
+		String sql = ""+
+				" delete from imooc_goddess "+
+				" where id=?"; 
+		PreparedStatement ptmt = conn.prepareStatement(sql);
+		
+		ptmt.setInt(1,id);
+		ptmt.execute(); 
+	}
+	
+	public void updateGoddess(Goddess g) throws SQLException {
+		Connection conn = DBUtil.getConnection();
+		String sql = ""+
+				" update  imooc_goddess "+
+				" set user_name=?,sex=?,age=?,birthday=?,email=?,mobile=?, "+
+				" update_user=?,update_date=current_date(),isdel=? "+
+				" where id=?"; 
+		PreparedStatement ptmt = conn.prepareStatement(sql);
+		
+		ptmt.setString(1, g.getUser_name());
+		ptmt.setInt( 2, g.getSex());
+		ptmt.setInt(3,g.getAge());
+		ptmt.setDate(4,new Date( g.getBirthday().getTime()));
+		ptmt.setString(5, g.getEmail());
+		ptmt.setString(6, g.getMobile());
+		ptmt.setString(7, g.getUpdate_user());
+		ptmt.setInt(8, g.getIsdel());
+		ptmt.setInt(9,g.getId());
+		ptmt.execute(); 
+	}
+	
+	public List<Goddess> query() throws Exception{
+		
+		Connection conn = DBUtil.getConnection();
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("select user_name,age from imooc_goddess");
+		
+		List<Goddess> gs = new ArrayList<Goddess>();
+		Goddess g = null;
+		while(rs.next()) {
+			g = new Goddess();
+			g.setUser_name(rs.getString("user_name"));
+			g.setAge(rs.getInt("age"));
+			gs.add(g);
+		} 
+		return gs;
+	}
+	
+	public Goddess get(Integer id) throws SQLException {
+		Goddess g = null;
+		Connection conn = DBUtil.getConnection();
+		String sql = ""+
+				" select * from imooc_goddess "+
+				" where id=?"; 
+		PreparedStatement ptmt = conn.prepareStatement(sql);
+		
+		ptmt.setInt(1,id);
+		ResultSet rs = ptmt.executeQuery(); 
+		while(rs.next()) {
+			g = new Goddess();
+			g.setId(rs.getInt("id"));
+			g.setUser_name(rs.getString("user_name"));
+			g.setAge(rs.getInt("age"));
+			g.setSex(rs.getInt("sex"));
+			g.setBirthday(rs.getDate("birthday"));
+			g.setEmail(rs.getString("email"));
+			g.setMobile(rs.getString("mobile"));
+			g.setCreate_date(rs.getDate("create_date"));
+			g.setCreate_user(rs.getString("create_user"));
+			g.setUpdate_date(rs.getDate("update_date"));
+			g.setUpdate_user(rs.getString("update_user"));
+			g.setIsdel(rs.getInt("isdel"));
+			
+		}
+		return g;
+	}
+	
+}
