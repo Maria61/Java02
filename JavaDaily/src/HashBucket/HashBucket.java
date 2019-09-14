9268b4eff7446c1180f3edefd795aa3d10e70efe
Maria61 <mariaLi61@outlook.com>
2019-09-14 11:03:17 +0800

    哈希表

--- JavaDaily/src/HashBucket/HashBucket.java
+++ JavaDaily/src/HashBucket/HashBucket.java
@@ -8,8 +8,27 @@ import javax.xml.soap.Node;
  * @date 2019/9/11 19:57
  */
 public class HashBucket {
+    private static class Node {
+        private int key;
+        private int val;
+        private Node next;
+    }
+
+    private Node[] array;
+    private int size;
+
+    public HashBucket() {
+        array = new Node[8];
+        size = 0;
+    }
+
+    private int hashFunction(int key, int capacity) {
+        return key % capacity;
+    }
+
     public int remove(int key) {
-        int index = hashFunction(key);
+
+        int index = hashFunction(key, array.length);
         Node head = array[index];
         Node cur = head;
         Node pre = null;
