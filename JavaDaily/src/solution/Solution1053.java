33e19c8024a246dd16721e2bcd525bf48fb2d818
Maria61 <mariaLi61@outlook.com>
2019-09-19 21:22:00 +0800

    添加题目描述

--- JavaDaily/src/solution/Solution1053.java
+++ JavaDaily/src/solution/Solution1053.java
@@ -10,6 +10,15 @@ import java.util.Arrays;
  */
 public class Solution1053 {
     //LeetCode 1053
+
+    /**
+     * @param A 一个正整数的数组 A（其中的元素不一定完全不同），
+     * @return 返回可在 一次交换（交换两数字 A[i] 和 A[j] 的位置）后得到的:按字典序排列小于 A 的最大可能排列。
+     * 如果无法这么操作，就请返回原数组。
+     * eg:
+     * 输入：[1,9,4,6,7]
+     * 输出：[1,7,4,6,9]
+     */
     public static int[] prevPermOpt1(int[] A) {
         int i;
         int c = 0;
@@ -25,7 +34,7 @@ public class Solution1053 {
         if (i == -1) {
             return A;
         }
-        //找出比A[c]小的数里的最大的数的下标
+        //找出比A[c]小的数里的最大的数的下标   ******有卡壳
         for (int j = c + 1; j < A.length; j++) {
             if (A[j] < A[c]) {
                 if (A[j] > max) {
--- JavaDaily/src/solution/Solution15.java
+++ JavaDaily/src/solution/Solution15.java
@@ -8,6 +8,21 @@ import java.util.*;
  * @date 2019/9/16 19:30
  */
 public class Solution15 {
+    /**
+     * 给定一个包含 n 个整数的数组 nums，
+     * 判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 。找出所有满足条件且不重复的三元组。
+     * 注意：答案中不可以包含重复的三元组。
+     * eg:
+     * nums = [-1, 0, 1, 2, -1, -4]
+     * 输出：
+     * [
+     * [-1, 0, 1],
+     * [-1, -1, 2]
+     * ]
+     *
+     * @param nums
+     * @return
+     */
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
