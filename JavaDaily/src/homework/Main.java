bc5a81dd91d51185be0b23dfc5b2f1297add5028
Maria61 <mariaLi61@outlook.com>
2019-09-20 21:52:40 +0800

    homework

---
+++ JavaDaily/src/homework/Main.java
@@ -0,0 +1,52 @@
+package homework;
+
+import java.util.Arrays;
+
+/**
+ * @author Maria
+ * @program JavaDaily
+ * @date 2019/9/20 19:01
+ */
+public class Main {
+
+    public static String judgeZodiac(int year) {
+
+        int start = 579;
+
+        if (year <= start) {
+            //据记载，南北朝时期十二生肖完全形成，设579年，十二生肖完全形成并扩展
+            return "该年份十二生肖尚未完全定型";
+        }
+        String[] zodiac = {"子鼠", "丑牛", "寅虎", "卯兔", "辰龙", "巳蛇",
+                "午马", "未羊", "申猴", "酉鸡", "戌狗", "亥猪"};
+        int c = (year - start) % 12;
+        if (c == 0) {
+            return zodiac[11];
+        }
+        return zodiac[c - 1];
+    }
+
+    public static int[] statGender(Student[] s, String major) {
+        int[] result = new int[2];//女生:0 , 男生:1
+        for (Student student : s) {
+            if (student.major.equals(major)) {
+                if (student.gender.equals("男")) {
+                    result[1] = result[1] + 1;
+                } else {
+                    result[0] = result[0] + 1;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(judgeZodiac(2019));
+        Student s1 = new Student("张三", "男", "计算机");
+        Student s2 = new Student("李四", "男", "网络");
+        Student s3 = new Student("王五", "男", "计算机");
+        Student s4 = new Student("赵六", "女", "计算机");
+        Student[] s = {s1, s2, s3, s4};
+        System.out.println(Arrays.toString(statGender(s, "网络")));
+    }
+}
---
+++ JavaDaily/src/homework/Student.java
@@ -0,0 +1,19 @@
+package homework;
+
+/**
+ * @author Maria
+ * @program JavaDaily
+ * @date 2019/9/20 18:58
+ */
+public class Student {
+
+    public String name;
+    public String gender;
+    public String major;
+
+    public Student(String name, String gender, String major) {
+        this.name = name;
+        this.gender = gender;
+        this.major = major;
+    }
+}
