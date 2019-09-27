815d166989d4f2aa11ddad97e71b6197beca5c8e
Maria61 <mariaLi61@outlook.com>
2019-09-27 23:19:25 +0800

    复习

---
+++ JavaDaily/src/review/EnumDemo.java
@@ -0,0 +1,21 @@
+package review;
+
+import java.util.Arrays;
+
+/**
+ * @author Maria
+ * @program JavaDaily
+ * @date 2019/9/27 18:55
+ */
+enum Week {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY,, FRIDAY, SATURDAY, SUNDAY
+}
+
+public class EnumDemo {
+    public static void main(String[] args) {
+        Week w = Week.MONDAY;
+        System.out.println(w);
+        System.out.println(Arrays.toString(Week.values()));
+    }
+
+}
---
+++ JavaDaily/src/review/Tree.java
@@ -0,0 +1,38 @@
+package review;
+
+import java.util.Stack;
+
+/**
+ * @author Maria
+ * @program JavaDaily
+ * @date 2019/9/27 20:34
+ */
+class MyNode {
+    int value;
+    MyNode left;
+    MyNode right;
+}
+
+public class Tree {
+    public boolean isSameTree(MyNode root1, MyNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        } else if (root1.value == root2.value) {
+            isSameTree(root1.left, root1.right);
+            isSameTree(root1.right, root2.right);
+        } else {
+            return false;
+        }
+        return false;
+    }
+
+    public void postOrder(MyNode root) {
+
+    }
+
+    public void levelOrder(MyNode root) {
+
+    }
+
+
+}
