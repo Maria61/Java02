6f2ea4f86e002f06eff911cd3c6d78bf8c5f8f5b
Maria61 <mariaLi61@outlook.com>
2019-11-08 00:00:29 +0800

    每日一题

--- JavaDaily/src/solution/SolutionDaily1107.java
+++ JavaDaily/src/solution/SolutionDaily1107.java
@@ -1,5 +1,8 @@
 package solution;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author Maria
  * @program JavaDaily
@@ -7,6 +10,10 @@ package solution;
  */
 public class SolutionDaily1107 {
     public static String longestNumString(String s) {
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < 10; i++) {
+            set.add();
+        }
         return null;
     }
 
