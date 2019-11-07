e96b60d39dfd6a0c1da3d886c7f04e3e843ef9a4
Maria61 <mariaLi61@outlook.com>
2019-11-07 23:54:37 +0800

    每日一题

---
+++ JavaDaily/src/solution/SolutionDaily1107.java
@@ -0,0 +1,16 @@
+package solution;
+
+/**
+ * @author Maria
+ * @program JavaDaily
+ * @date 2019/11/7 23:52
+ */
+public class SolutionDaily1107 {
+    public static String longestNumString(String s) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
