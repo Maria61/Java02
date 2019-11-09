a8b182169a76cc596a1c800c4a8f20cf36728269
Maria61 <mariaLi61@outlook.com>
2019-11-10 00:03:00 +0800

    每日一题

---
+++ JavaDaily/src/solution/SolutionDaily1109.java
@@ -0,0 +1,27 @@
+package solution;
+
+import java.util.Stack;
+
+/**
+ * @author Maria
+ * @program JavaDaily
+ * @date 2019/11/9 23:38
+ */
+public class SolutionDaily1109 {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        if (!stack2.empty()) {
+            return stack2.pop();
+        }
+        while (stack1.empty()) {
+            stack2.push(stack1.pop());
+        }
+        return stack2.pop();
+    }
+}
