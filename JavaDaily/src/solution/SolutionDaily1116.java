7f33d352ce94f7210a55e9520878e3162c7161f4
Maria61 <mariaLi61@outlook.com>
2019-11-18 12:56:40 +0800

    每日一题

--- JavaDaily/src/solution/SolutionDaily1116.java
+++ JavaDaily/src/solution/SolutionDaily1116.java
@@ -6,6 +6,24 @@ package solution;
  * @date 2019/11/16 17:34
  */
 public class SolutionDaily1116 {
+    public int[] multiply(int[] A) {
+        int[] B = new int[A.length];
+        for (int i = 0; i < B.length; i++) {
+            B[i] = mul(A, i);
+        }
+        return B;
+    }
+
+    private int mul(int[] a, int i) {
+        int re = 1;
+        for (int j = 0; i < a.length; j++) {
+            if (j != i) {
+                re *= a[j];
+            }
+        }
+        return re;
+    }
+
     public static void main(String[] args) {
     }
 }
---
+++ JavaDaily/src/solution/SolutionDaily1117.java
@@ -0,0 +1,20 @@
+package solution;
+
+/**
+ * @author Maria
+ * @program JavaDaily
+ * @date 2019/11/18 12:44
+ */
+public class SolutionDaily1117 {
+    public int[] exchangeAB(int[] AB) {
+        // write code here
+        AB[0] = AB[0] + AB[1];
+        AB[1] = AB[0] - AB[1];
+        AB[0] = AB[0] - AB[1];
+        return AB;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
