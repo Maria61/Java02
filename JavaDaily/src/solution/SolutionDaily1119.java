c90107941ee3af629178f253c5115ceb38ff817b
Maria61 <mariaLi61@outlook.com>
2019-11-19 22:55:34 +0800

    每日一题

---
+++ JavaDaily/src/solution/SolutionDaily1119.java
@@ -0,0 +1,15 @@
+package solution;
+
+/**
+ * @author Maria
+ * @program JavaDaily
+ * @date 2019/11/19 22:52
+ */
+public class SolutionDaily1119 {
+    public static void main(String[] args) {
+        int i = 0;
+//        for( i == 0;true;true){//不是语句，执行不了！！！
+//
+//        }
+    }
+}
