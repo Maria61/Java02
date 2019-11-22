e4a65aa913f740044752a1156adc7536afc2a9c7
Maria61 <mariaLi61@outlook.com>
2019-11-22 09:52:56 +0800

    人工智能——产生式系统

--- homework/src/artificial_intelligence/VegetableClassify.java
+++ homework/src/artificial_intelligence/VegetableClassify.java
@@ -14,31 +14,133 @@ public class VegetableClassify {
      * 土豆、南瓜、番茄、黄瓜、茄子、苦瓜、冬瓜、白菜、青菜、紫甘蓝、西兰花、
      * 菜花、豆角、青椒、蘑菇、香菇、红薯、胡萝卜🥕
      * 木耳、金针菇
-     * @param args
      */
 
+    /**
+     * "1.红色的、2.黄色的、3.绿色的、4.紫色的、" +
+     * "5.白色的、6.黑色的、7.棕色的、8.灰色的"+"\n"+
+     * "9.甜的、10.辣的、11.苦的、12.酸的、13.青涩味、14.无味"+"\n"+
+     * "15.圆的、" +"16.长条的、"+"\n"+
+     * "16.不可生食的、17.可生食的、"+"\n"+
+     * "18.包含可食豆类的、19.菌类、
+     *
+     * 颜色、形状、味道、是否可生食
+     */
 
-    public static void main(String[] args) {
-        final int NUM = 18;
+    static String[] feature = {"红色的","黄色的","绿色的","紫色的",
+            "白色的","黑色的","棕色的","灰色的",
+            "甜的","辣的","苦的","酸的","青涩味","无味",
+            "圆的","长条的","菜花状",
+            "不可生食的","可生食的"};
+    static String[] vegetable = {"土豆","南瓜","番茄","黄瓜","茄子","苦瓜","冬瓜","白菜","青菜","紫甘蓝","西兰花",
+            "菜花","豆角","青椒","蘑菇","香菇","红薯","胡萝卜","木耳","金针菇"};
+    private static void rules(int[] fact) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("蔬菜的特征如下：1.红色的、2.黄色的、3.绿色的、4.紫色的、" +
-                            "5.白色的、"+"\n"+"6.黑色的、7.棕色的、8.灰色的" +
-                            "9.甜的、10.辣的、"+"\n"+"11.苦的、12.酸的、13.圆的、" +
-                            "14.长条的、15.菌类、"+"\n"+"16.可生食的、17.包含可食豆类的、" +
-                            "18.青涩味的、19.不可生食的");
-        int[] fact = new int[NUM];
-        String[] feature = {"红色的","黄色的","绿色的","紫色的","白色的","黑色的","棕色的","灰色的",
-                "甜的","辣的","苦的","酸的","圆的","长条的","菌类","可生食的","包含可食豆类的","青涩味的",
-                "不可生食的"};
-        String[] vegetable = {"土豆","南瓜","番茄","黄瓜","茄子","苦瓜","冬瓜","白菜","青菜","紫甘蓝","西兰花",
-                "菜花","豆角","青椒","蘑菇","香菇","红薯","胡萝卜"};
-        System.out.println("请输入描述该蔬菜特征的个数(0<num<18):");
-        int num = sc.nextInt();
-        System.out.println("请输入该蔬菜的特征描述的序号(按序号由小到大):");
-        for(int i = 0 ; i < num; i++){
+        if (feature[fact[0] - 1].equals("红色的")) {
+            if (feature[fact[1] - 1].equals("圆的")) {
+                if (feature[fact[2] - 1].equals("酸的")) {
+                    if (feature[fact[3] - 1].equals("可生食的")) {
+                        System.out.println("结果为：番茄");
+                        return;
+                    }
+                }
+            }
+        }
+        if (feature[fact[0] - 1].equals("黄色的")) {
+            if (feature[fact[1] - 1].equals("圆的")) {
+                if (feature[fact[2] - 1].equals("无味")) {
+                    if (feature[fact[3] - 1].equals("不可生食的")) {
+                        System.out.println("结果为：土豆");
+                        return;
+                    }
+                }
+            }
+        }
+        if (feature[fact[0] - 1].equals("黄色的")) {
+            if (feature[fact[1] - 1].equals("圆的")) {
+                if (feature[fact[2] - 1].equals("甜的")) {
+                    if (feature[fact[3] - 1].equals("不可生食的")) {
+                        System.out.println("结果为：南瓜");
+                        return;
+                    }
+                }
+            }
+        }
+        if (feature[fact[0] - 1].equals("绿色的")) {
+            if (feature[fact[1] - 1].equals("长条的")) {
+                if (feature[fact[2] - 1].equals("青涩味")) {
+                    if (feature[fact[3] - 1].equals("可生食的")) {
+                        System.out.println("结果为：黄瓜");
+                        return;
+                    }
+                }
+            }
+        }
+        if (feature[fact[0] - 1].equals("绿色的")) {
+            if (feature[fact[1] - 1].equals("长条的")) {
+                if (feature[fact[2] - 1].equals("无味")) {
+                    if (feature[fact[3] - 1].equals("不可生食的")) {
+                        System.out.println("条件不足，请选择特征：");
+                        System.out.println("1.包含可食豆类的  2.不包含可食豆类的");
+                        int f = sc.nextInt();
+                        if(f == 1){
+                            System.out.println("结果为：豆角");
+                        }else{
+                            System.out.println("结果为：冬瓜");
+                        }
+                        return;
+                    }
+                }
+            }
+        }
+        if (feature[fact[0] - 1].equals("绿色的")) {
+            if (feature[fact[1] - 1].equals("长条的")) {
+                if (feature[fact[2] - 1].equals("苦味")) {
+                    if (feature[fact[3] - 1].equals("可生食的")) {
+                        System.out.println("结果为：苦瓜");
+                        return;
+                    }
+                }
+            }
+        }
+        if (feature[fact[0] - 1].equals("紫色的")) {
+            if (feature[fact[1] - 1].equals("长条的")) {
+                if (feature[fact[2] - 1].equals("无味")) {
+                    if (feature[fact[3] - 1].equals("不可生食的")) {
+                        System.out.println("结果为：茄子");
+                        return;
+                    }
+                }
+            }
+        }
+        if (feature[fact[0] - 1].equals("绿色的")) {
+            if (feature[fact[1] - 1].equals("长条的")) {
+                if (feature[fact[2] - 1].equals("苦味")) {
+                    if (feature[fact[3] - 1].equals("可生食的")) {
+                        System.out.println("结果为：苦瓜");
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println("现有事实无法推断出结果！");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("蔬菜的特征如下：" +"\n"+
+                            "1.红色的、2.黄色的、3.绿色的、4.紫色的、" +
+                            "5.白色的、6.黑色的、7.棕色的、8.灰色的"+"\n"+
+                            "9.甜的、10.辣的、11.苦的、12.酸的、13.青涩味、14.无味"+"\n"+
+                            "15.圆的、16.长条的、17.菜花状"+"\n"+
+                            "18.不可生食的、19.可生食的、"+"\n");
+        int[] fact = new int[4];
+        System.out.println("请依次输入四个数字表述蔬菜四个特征(颜色、形状、味道、是否可生食):");
+        for(int i = 0 ; i < 4; i++){
             fact[i] = sc.nextInt();
         }
-
+        rules(fact);
 
     }
+
+
 }
