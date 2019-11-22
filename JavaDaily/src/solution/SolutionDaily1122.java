0e79571a6c52ef181620625ac217a3a1a3d9d39b
Maria61 <mariaLi61@outlook.com>
2019-11-23 00:58:30 +0800

    每日一题

---
+++ JavaDaily/src/solution/SolutionDaily1122.java
@@ -0,0 +1,17 @@
+package solution;
+
+/**
+ * @author Maria
+ * @program JavaDaily
+ * @date 2019/11/22 23:39
+ */
+public class SolutionDaily1122 {
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 0;
+        if (++i > 0 || ++j > 0) {
+            System.out.println(i);
+            System.out.println(j);
+        }
+    }
+}
