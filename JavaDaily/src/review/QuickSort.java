bba8f3b8255586575fe39fc9e4b9f43c23147b00
Maria61 <mariaLi61@outlook.com>
2019-11-26 21:35:52 +0800

    快排

--- JavaDaily/src/review/QuickSort.java
+++ JavaDaily/src/review/QuickSort.java
@@ -1,5 +1,7 @@
 package review;
 
+import java.util.Arrays;
+
 /**
  * @author Maria
  * @program JavaDaily
@@ -7,7 +9,7 @@ package review;
  */
 public class QuickSort {
     public static void quickSort(int[] array) {
-        quickSortIntenel(array, 0, array.length);
+        quickSortIntenel(array, 0, array.length - 1);
     }
 
     private static void quickSortIntenel(int[] array, int left, int right) {
@@ -19,14 +21,33 @@ public class QuickSort {
         quickSortIntenel(array, p + 1, right);
     }
 
-//    private static int partition(int[] array, int left, int right) {
-//
-//
-//    }
+    private static int partition(int[] array, int left, int right) {
+        int less = left;
+        int great = right;
+        int p = array[right];
+        while (less < great) {
+            if (less < great && array[less] <= p) {
+                less++;
+            }
+            if (less < great && array[great] >= p) {
+                great--;
+            }
+            int t = array[less];
+            array[less] = array[great];
+            array[great] = t;
+        }
+        int t = array[less];
+        array[less] = array[great];
+        array[great] = t;
+
+        return 0;
+    }
 
 
 
     public static void main(String[] args) {
-
+        int[] array = {3, 4, 2, 6, 5, 1, 8, 3};
+        quickSort(array);
+        System.out.println(Arrays.toString(array));
     }
 }
