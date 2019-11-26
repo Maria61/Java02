82f35a1acb040151545dda340c25027d946e87fe
Maria61 <mariaLi61@outlook.com>
2019-11-26 18:41:13 +0800

    快排

---
+++ JavaDaily/src/review/QuickSort.java
@@ -0,0 +1,22 @@
+package review;
+
+/**
+ * @author Maria
+ * @program JavaDaily
+ * @date 2019/11/26 18:07
+ */
+public class QuickSort {
+    public static void quickSort(int[] array) {
+        quickSortIntenel(array, 0, array.length);
+    }
+
+    private static void quickSortIntenel(int[] array, int left, int right) {
+        int p = array[right];
+        partinal(array, 0, )
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
