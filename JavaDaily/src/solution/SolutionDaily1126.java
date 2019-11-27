4d052ed59cbb158adea40c6f80c9361fdcf639f9
Maria61 <mariaLi61@outlook.com>
2019-11-28 00:21:48 +0800

    每日一题

--- JavaDaily/src/solution/SolutionDaily1126.java
+++ JavaDaily/src/solution/SolutionDaily1126.java
@@ -1,5 +1,9 @@
 package solution;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 /**
  * @author Maria
  * @program JavaDaily
@@ -16,16 +20,21 @@ public class SolutionDaily1126 {
 
     public static String[] sort(String[] s, boolean b) {
         String[] re = new String[s.length];
+        Map<String, Integer> map = new TreeMap<>();
         for (int i = 0; i < s.length; i++) {
             String[] s1 = s[i].split(" ");
-            for (int j = 0; j < re.length; j++) {
-                if (s1[1] < re[])
-            }
+            map.put(s1[0], Integer.valueOf(s1[1]));
         }
         return null;
     }
 
     public static void main(String[] args) {
-
+        Map<String, Integer> map = new TreeMap<>();
+        map.put("acb", 12);
+        map.put("sdf", 12);
+        map.put("c", 13);
+        for (Map.Entry<String, Integer> m : map.entrySet()) {
+            System.out.println(m.getKey() + "," + m.getValue());
+        }
     }
 }
