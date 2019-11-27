218b9c62547c82ef68f0f21143bf2010fd885623
Maria61 <mariaLi61@outlook.com>
2019-11-27 16:39:02 +0800

    八皇后问题

---
+++ homework/src/algorithm_experiment/Experiment1127.java
@@ -0,0 +1,37 @@
+package algorithm_experiment;
+
+/**
+ * @author Maria
+ * @program homework
+ * @date 2019/11/27 16:26
+ */
+public class Experiment1127 {
+    static int[] queen = new int[9];
+    public static void nQueen(int now){
+        int i;
+        if(now==9){
+            for(i=0;i<9;i++){
+                System.out.print(queen[i]+" ");
+            }
+            System.out.println();
+
+            return;
+        }
+
+        int j;
+        for(i=0;i<9;i++){//i表示正在放的皇后的位置，即该皇后的纵坐标
+            for(j=0;j<now;j++){//j表示已经放好的皇后的编号
+                if(i==queen[j]||Math.abs(now-j)==Math.abs(i-queen[j])){
+                    break;
+                }
+            }
+            if(j==now){
+                queen[now]=i;
+                nQueen(now+1);
+            }
+        }
+    }
+    public static void main(String[] args) {
+            nQueen(0);
+    }
+}
