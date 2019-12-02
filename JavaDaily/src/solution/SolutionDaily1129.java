c6c89fdf908fb33bdede32aaae72242165cafce8
Maria61 <mariaLi61@outlook.com>
2019-12-02 09:18:56 +0800

    每日一题

--- JavaDaily/src/solution/SolutionDaily1129.java
+++ JavaDaily/src/solution/SolutionDaily1129.java
@@ -9,7 +9,68 @@ import java.util.Scanner;
  * @program JavaDaily
  * @date 2019/12/1 23:46
  */
+
 public class SolutionDaily1129 {
+
+    public static ListNode plusAB(ListNode a, ListNode b) {
+        ListNode re = null;
+        ListNode cura = a;
+        ListNode curb = b;
+        ListNode cur = re;
+        int m = 0;
+        int s = 0;
+        while (cura != null && curb != null) {
+            s = cura.val + curb.val + m;
+            m = s / 10;
+            ListNode n = new ListNode(s % 10);
+            if (re == null) {
+                re = n;
+                cur = n;
+            } else {
+                cur.next = n;
+                cur = cur.next;
+            }
+
+            cura = cura.next;
+            curb = curb.next;
+        }
+        while (cura != null) {
+            s = cura.val + m;
+            m = s / 10;
+            ListNode n = new ListNode(s % 10);
+            if (re == null) {
+                re = n;
+                cur = n;
+            } else {
+                cur.next = n;
+                cur = cur.next;
+            }
+
+            cura = cura.next;
+        }
+        while (curb != null) {
+            s = curb.val + m;
+            m = s / 10;
+            ListNode n = new ListNode(s % 10);
+            if (re == null) {
+                re = n;
+                cur = n;
+            } else {
+                cur.next = n;
+                cur = cur.next;
+            }
+            curb = curb.next;
+
+        }
+
+        if (m != 0) {
+            ListNode n = new ListNode(m);
+            cur.next = n;
+        }
+
+        return re;
+    }
+
     public static int judge(String s1, String s2) {
         int re = 0;
         char[] c2 = s2.toCharArray();
@@ -17,7 +78,7 @@ public class SolutionDaily1129 {
         for (int i = 0; i < c2.length; i++) {
             if (map2.containsKey(c2[i])) {
                 int v = map2.get(c2[i]);
-                map2.put(c2[i], v++);
+                map2.put(c2[i], ++v);
             } else {
                 map2.put(c2[i], 1);
             }
@@ -27,7 +88,7 @@ public class SolutionDaily1129 {
         for (int i = 0; i < c1.length; i++) {
             if (map1.containsKey(c1[i])) {
                 int v = map1.get(c1[i]);
-                map1.put(c1[i], v++);
+                map1.put(c1[i], ++v);
             } else {
                 map1.put(c1[i], 1);
             }
@@ -46,14 +107,31 @@ public class SolutionDaily1129 {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s1 = sc.nextLine();
-        String s2 = sc.nextLine();
-        int re = judge(s1, s2);
-        if (re == 0) {
-            System.out.println("Yes " + (s1.length() - s2.length()));
-        } else {
-            System.out.println("No " + re);
+//        Scanner sc = new Scanner(System.in);
+//        String s1 = sc.nextLine();
+//        String s2 = sc.nextLine();
+//        int re = judge(s1, s2);
+//        if (re == 0) {
+//            System.out.println("Yes " + (s1.length() - s2.length()));
+//        } else {
+//            System.out.println("No " + re);
+//        }
+
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(9);
+        ListNode n3 = new ListNode(9);
+        ListNode n4 = new ListNode(9);
+        ListNode n5 = new ListNode(9);
+        n1.next = null;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+
+        ListNode re = plusAB(n1, n2);
+        ListNode cur = re;
+        while (cur != null) {
+            System.out.print(cur.val + "->");
+            cur = cur.next;
         }
     }
 }
