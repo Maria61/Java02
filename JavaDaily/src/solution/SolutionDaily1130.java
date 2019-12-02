58cea422da9fa46d5568cfa44c7e9ac0f7298c84
Maria61 <mariaLi61@outlook.com>
2019-12-02 16:18:01 +0800

    每日一题

--- JavaDaily/src/solution/SolutionDaily1130.java
+++ JavaDaily/src/solution/SolutionDaily1130.java
@@ -11,8 +11,26 @@ import java.util.Scanner;
 public class SolutionDaily1130 {
     static boolean[] flag = new boolean[5];
 
+    //判断是否是平衡二叉树
     public boolean isBalance(TreeNode root) {
-        return false;
+        if (root == null) {
+            return true;
+        }
+        int left = depth(root.left);
+        int right = depth(root.right);
+        if (Math.abs(left - right) > 1) {
+            return false;
+        }
+        return true;
+    }
+
+    private int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = depth(root.left);
+        int right = depth(root.right);
+        return left > right ? (left + 1) : (right + 1);
     }
 
     public static double[] judge(String s) {
