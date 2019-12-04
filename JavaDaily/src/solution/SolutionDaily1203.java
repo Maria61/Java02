ece342d567a53ed7ae5f4a9a46a5f6f433f543ac
Maria61 <mariaLi61@outlook.com>
2019-12-04 11:06:43 +0800

    每日一题

--- JavaDaily/src/solution/SolutionDaily1203.java
+++ JavaDaily/src/solution/SolutionDaily1203.java
@@ -51,11 +51,7 @@ public class SolutionDaily1203 {
         return (double) GPA;
     }
 
-    public static String RMB(double money) {
-        return null;
-    }
-
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] credits = new int[n];
@@ -67,5 +63,58 @@ public class SolutionDaily1203 {
             grade[j] = sc.nextInt();
         }
         System.out.printf("%.2f", calculateGPA(n, credits, grade));
+    }
+
+    public static String RMB(double money) {
+        String s = String.valueOf(money);
+        char[] c = s.toCharArray();
+        int flag = 0;
+        for (int i = 0; i < c.length; i++) {
+            if (c[i] == '.') {
+                flag = i;
+                break;
+            }
+        }
+        int f = flag;
+        char[] r = {'零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖'};
+        String[] w = {"元", "拾", "佰", "仟", "万", "拾", "佰", "仟", "亿"};
+        char[] y = {'角', '分'};
+        String re = new String();
+        boolean b = false;
+        for (int i = 0; flag > 0 && i < c.length; i++) {
+            if (c[i] != '0') {
+                //处理十位为0，个位不为0的情况
+                if (i != 0 && c[i - 1] == '0') {
+                    re += r[0];
+                }
+                re += r[c[i] - '0'];
+                re += w[flag - 1];
+            } else if (i == (f - 1)) {//处理个位为0的情况
+                re += w[0];
+            }
+            flag--;
+        }
+        for (int j = f + 1, i = 0; j < c.length && j < (f + 3); j++, i++) {
+            if (c[j] != '0') {
+                if (j != f + 1 && c[j - 1] == '0') {
+                    re += r[0];
+                }
+                re += r[c[j] - '0'];
+                re += y[i];
+            }
+        }
+        if (f + 2 == c.length) {
+            if (c[f + 1] == '0') {
+                re += "整";
+            }
+        }
+        return re;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double y = sc.nextDouble();
+        System.out.println("人名币" + RMB(y));
     }
 }
