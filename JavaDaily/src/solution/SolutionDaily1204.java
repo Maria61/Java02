a56ba0826eb8b832c72a4106f08297403e6a22fb
Maria61 <mariaLi61@outlook.com>
2019-12-05 17:59:17 +0800

    每日一题

--- JavaDaily/src/solution/SolutionDaily1204.java
+++ JavaDaily/src/solution/SolutionDaily1204.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  * @date 2019/12/5 16:45
  */
 public class SolutionDaily1204 {
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
             int n = sc.nextInt();
@@ -27,4 +27,54 @@ public class SolutionDaily1204 {
         }
         return re;
     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] candidate = new String[n];
+        String s = sc.nextLine();
+        addCandidate(s, candidate);
+        int v = sc.nextInt();
+        sc.nextLine();
+        String[] vote = new String[v];
+        s = sc.nextLine();
+        Vote(s, vote);
+        printResult(candidate, vote);
+    }
+
+    private static void printResult(String[] candidate, String[] vote) {
+        int invalid = vote.length;
+        int[] v = new int[candidate.length];
+        for (int i = 0; i < vote.length; i++) {
+            for (int j = 0; j < candidate.length; j++) {
+                if (vote[i].equals(candidate[j])) {
+                    v[j]++;
+                    invalid--;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < v.length; i++) {
+            System.out.println(candidate[i] + " : " + v[i]);
+        }
+        System.out.println("Invalid : " + invalid);
+
+    }
+
+    private static void Vote(String s, String[] vote) {
+        String[] c = s.split(" ");
+        for (int i = 0; i < vote.length; i++) {
+            vote[i] = c[i];
+        }
+    }
+
+    private static void addCandidate(String s, String[] candidate) {
+        String[] c = s.split(" ");
+        for (int i = 0; i < candidate.length; i++) {
+            candidate[i] = c[i];
+        }
+    }
+
+
 }
