3342bffcb145a2070602f473e53f4968e28edf98
Maria61 <mariaLi61@outlook.com>
2019-12-16 22:08:55 +0800

    Http解包并响应请求

--- JavaDaily/src/net/HttpServer.java
+++ JavaDaily/src/net/HttpServer.java
@@ -27,45 +27,6 @@ public class HttpServer {
             while (true) {
                 //获取客户端请求socket对象
                 Socket socket = server.accept();//阻塞式方法
-
-                //如果获取到请求对象，开启一个线程进行处理，否则一直阻塞不处理
-//                EXE.submit(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        try {
-//                            //输入流处理请求
-//                            InputStream is = socket.getInputStream();
-//                            //转换流（字节流转换为字符流）
-//                            BufferedReader br = new BufferedReader(
-//                                    new InputStreamReader(is,"UTF-8"));
-//                            //输出流响应请求
-//                            OutputStream os = socket.getOutputStream();
-//                            BufferedWriter bw = new BufferedWriter(
-//                                    new OutputStreamWriter(os,"UTF-8"));
-//
-//                            String requestLine = br.readLine();//读取请求行//readLine()阻塞式方法,阻塞main主线程
-//
-//                            Request requst = new Request();
-//                            String[] requestLines = requestLine.split(" ");
-//                            requst.setMethod(requestLines[0]);
-//                            requst.setUrl(requestLines[1]);
-//                            requst.setVersion(requestLines[2]);
-//                            //处理请求头
-//                            String requestHeader;
-//                            while ((requestHeader = br.readLine()).length() != 0) {
-//                                String[] headers = requestHeader.split(":");
-//                                requst.addHeaders(headers[0], headers[1].trim());//字符串去除前后空格
-//                            }
-//                            System.out.println(requst);
-//                            String requestParameter;
-//                            char[] chars = new char[1024];
-//                            br.readLine();
-//                        } catch (IOException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                });
-
                 EXE.submit(new HttpTask(socket));
 
             }
