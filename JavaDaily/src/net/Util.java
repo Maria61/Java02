01cabc935cc21afb1617b30bad915148155366f9
Maria61 <mariaLi61@outlook.com>
2019-12-24 22:02:52 +0800

    apache

---
+++ JavaDaily/src/net/Util.java
@@ -0,0 +1,13 @@
+package net;
+
+/**
+ * @author Maria
+ * @program JavaDaily
+ * @date 2019/12/24 21:59
+ */
+//????
+public class Util {
+    public static void main(String[] args) {
+        HanyuPinyinOutputFormat format = new HanyuPinyinOutputFormat();
+    }
+}
