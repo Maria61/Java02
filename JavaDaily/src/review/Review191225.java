da8a3a9d95bd522716f0fc43a8c140381c18e033
Maria61 <mariaLi61@outlook.com>
2019-12-25 16:08:35 +0800

    review_char

---
+++ JavaDaily/src/review/Review191225.java
@@ -0,0 +1,18 @@
+package review;
+
+/**
+ * @author Maria
+ * @program JavaDaily
+ * @date 2019/12/25 15:24
+ */
+public class Review191225 {
+    public static void main(String[] args) {
+//        char ch = (char)true;//boolean不能转化为char
+//        char ch1 = 'sadfaf';//字符文字中字符太多
+        char ch3 = '\111';
+//        char ch4 = '\u123';//非法转义字符，即使注掉也编译不过
+        System.out.println(ch3);
+
+
+    }
+}
