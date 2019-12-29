a790ff7432ce96dfb88d4b520b82f1ae31b7ab52
Maria61 <mariaLi61@outlook.com>
2019-12-29 20:14:50 +0800

    操作系统实验

---
+++ homework/src/operating_system_experiment/ex4/Main.java
@@ -0,0 +1,28 @@
+package operating_system_experiment.ex4;
+import java.util.Scanner;
+
+/**
+ * @author Maria
+ * @program homework
+ * @date 2019/12/29 18:21
+ */
+//主程序入口
+public class Main {
+    private static Scanner scanner =new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.print("请输入起始磁道号:");
+        int start=scanner.nextInt();
+        System.out.print("请输入终止磁道号：");
+        int end=scanner.nextInt();
+        System.out.println("请输入磁盘请求数大小（当前磁头在100）");
+        int num=scanner.nextInt();
+        Shedule shedule=new Shedule(start,end,num);
+        System.out.println("生成的随机磁道请求号为：");
+        shedule.setRandomRequests();
+        float avglen=shedule.realSCAN();
+        System.out.println("平均寻道长度:"+avglen);
+    }
+}
+
+
+
--- homework/src/operating_system_experiment/ex4/Process.java
+++
@@ -1,6 +0,0 @@
-package operating_system_experiment.ex4;
-
-public class Process {
-    private boolean status;//true:工作，false:等待
-
-}
