a719346a6fabf80d15da496373a5c3ae0eef1327
Maria61 <mariaLi61@outlook.com>
2020-01-07 12:51:05 +0800

    寒假复习

---
+++ JavaDaily/src/review/winter/Day01.java
@@ -0,0 +1,15 @@
+package review.winter;
+
+/**
+ * @author Maria
+ * @program JavaDaily
+ * @date 2020/1/7 12:46
+ */
+public class Day01 {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 5;
+        System.out.println(x+y+""+(x+y)+y);
+        System.out.println(x+""+y);//""将x+y表达式分割为x与y的连接
+    }
+}
