57e5c9352c96e6023fcfa3e0b716fb3779ad79aa
Maria61 <mariaLi61@outlook.com>
2020-02-10 20:44:49 +0800

    寒假复习

--- JavaDaily/src/review/winter/Day02.java
+++ JavaDaily/src/review/winter/Day02.java
@@ -1,5 +1,7 @@
 package review.winter;
 
+import java.util.Arrays;
+
 /**
  * @author Maria
  * @program JavaDaily
@@ -7,8 +9,29 @@ package review.winter;
  */
 public class Day02 {
 
+    public static int removeElement(int[] nums, int val){
+        int flag = -1;
+        int count = nums.length;
+        for(int  i = 0,j = -1; i < nums.length;i++){
+            if(nums[i] == val && j == -1){
+                j = i;
+            }
+            if(nums[i] != val && j != -1){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+
+        }
+        System.out.println(Arrays.toString(nums));
+        return count;
+    }
+
+
 
     public static void main(String[] args) {
-        System.out.println("水逆的一天！！！！");
+        int[] nums = {0,1,2,2,3,0,4,2};
+        System.out.println(removeElement(nums,2));
     }
 }
