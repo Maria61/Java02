93291ffec607716d97c59fb84c5a4b0801750578
Maria61 <mariaLi61@outlook.com>
2020-02-20 23:22:57 +0800

    everything-like☞

--- everything-like/src/main/java/task/FileSave.java
+++ everything-like/src/main/java/task/FileSave.java
@@ -1,10 +1,13 @@
 package task;
 
+import app.FileMeta;
 import util.DBUtil;
 import util.PinyinUtil;
 
 import java.io.File;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Maria
@@ -15,13 +18,48 @@ public class FileSave implements ScanCallback{
     @Override
     public void callback(File dir) {
         File[] children = dir.listFiles();
+        List<FileMeta> locals = new ArrayList<>();
         if(children != null) {
-            for (File chile : children) {
-                System.out.println(chile.getPath());
-                save(chile);
+            for (File child : children) {
+                locals.add(new FileMeta(child));
             }
         }
     }
+
+
+    private List<FileMeta> query(File dir){
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<FileMeta> metas = new ArrayList<>();
+        try{
+            connection = DBUtil.getConnection();
+            String sql = "select name, path, is_directory, size, last_modified" +
+                    " from file_meta where path = ?";
+             ps = connection.prepareStatement(sql);//预编译
+             ps.setString(1,dir.getPath());
+             rs = ps.executeQuery();
+             while(rs.next()){
+                 String name = rs.getString("name");
+                 String path = rs.getString("path");
+                 Boolean isDirectory = rs.getBoolean("is_directory");
+                 Long size = rs.getLong("size");
+                 Timestamp lastModified = rs.getTimestamp("last_modified");
+                 FileMeta meta = new FileMeta(name, path, isDirectory, size,
+                         new java.util.Date(lastModified.getTime()));
+                 System.out.println(meta);
+                 metas.add(meta);
+             }
+            return metas;
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new RuntimeException("查询文件信息出错",e);
+        }finally{
+            DBUtil.close(connection,ps,rs);
+        }
+
+    }
+
 
     /**
      * 将扫描结果存入数据库
@@ -36,13 +74,14 @@ public class FileSave implements ScanCallback{
             connection = DBUtil.getConnection();
             //2.创建sql语句执行对象
             String sql = "insert into file_meta" +
-                    " (name,path,size,last_modified,pinyin,pinyin_first)" +
-                    " values (?,?,?,?,?,?)";
+                    " (name,path,is_directory,size,last_modified,pinyin,pinyin_first)" +
+                    " values (?,?,?,?,?,?,?)";
             statement = connection.prepareStatement(sql);
             statement.setString(1,file.getName());
             statement.setString(2,file.getParent());
-            statement.setLong(3,file.length());
-            statement.setTimestamp(4,new Timestamp(file.lastModified()));//上次的修改时间
+            statement.setBoolean(3,file.isDirectory());
+            statement.setLong(4,file.length());
+            statement.setTimestamp(5,new Timestamp(file.lastModified()));//上次的修改时间
             String pinyin = null;
             String pinyin_first = null;
             //文件名中包含汉字才有拼音及拼音首字母
@@ -51,8 +90,8 @@ public class FileSave implements ScanCallback{
                 pinyin = pinyins[0];
                 pinyin_first = pinyins[1];
             }
-            statement.setString(5,pinyin);
-            statement.setString(6,pinyin_first);
+            statement.setString(6,pinyin);
+            statement.setString(7,pinyin_first);
 //            System.out.println(sql);
             //3.执行sql
             statement.executeUpdate();
--- everything-like/src/main/java/util/Util.java
+++ everything-like/src/main/java/util/Util.java
@@ -1,5 +1,8 @@
 package util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author Maria
  * @program everything-like
@@ -7,4 +10,23 @@ package util;
  */
 public class Util {
     public static final String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+    public static String parseSize(Long size) {
+        String[] danweis = {"B","KB","MB","GB","PB","TB"};
+        int index = 0;
+        while(size > 1024 && index < danweis.length - 1){
+            size /= 1024;
+            index++;
+        }
+        return size +danweis[index];
+    }
+
+    public static String parseDate(Date lastModified) {
+        return new SimpleDateFormat(DATE_PATTERN).format(lastModified);
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 }
