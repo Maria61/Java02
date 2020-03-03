1c970415e9ad8f70dad0551e19ce33e6b46a8e0c
Maria61 <mariaLi61@outlook.com>
2020-03-03 12:43:21 +0800

    web-tomcat

---
+++ homework2020/mylProject/src/com/MyServlet.java
@@ -0,0 +1,27 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author Maria
+ * @program mylProject
+ * @date 2020/3/3 11:17
+ */
+public class MyServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+//        response.setContentType("text/html; charset=UTF-8");
+//        PrintWriter pw = response.getWriter();
+//        for(int i = 1; i <= 9; i++){
+//            for(int j = 1; j <= i; j++){
+//                pw.print(i+"*"+j+"="+i*j+" ");
+//            }
+//            pw.println("<br>");
+//        }
+//        pw.flush();
+    }
+}
