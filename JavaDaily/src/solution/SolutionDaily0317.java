79db04dd456646ba6a75bebfc123d1225519c44d
Maria61 <mariaLi61@outlook.com>
2020-03-19 14:44:55 +0800

    LeetCode 219 存在重复元素II

--- JavaDaily/src/solution/SolutionDaily0317.java
+++ JavaDaily/src/solution/SolutionDaily0317.java
@@ -8,11 +8,39 @@ import java.util.*;
  * @date 2020/3/18 8:46
  */
 public class SolutionDaily0317 {
-    public static void main(String[] args) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(Map.Entry<Character,Integer> m : map.entrySet()){
 
+    /**
+    *LeetCode 219 判断给定数组中，是否有两个相同的元素，且间距差小于等于k
+     */
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        // if(nums.length == 1){
+        //     return false;
+        // }
+        if(k < nums.length){
+            for(int i = 0; i < nums.length; i++){
+                for(int j = 1; j <= k; j++){
+                    if((i + j) < nums.length && nums[i] == nums[i + j]){
+                        return true;
+                    }
+                }
+
+            }
+        }else{
+            for(int i = 0; i < nums.length; i++){
+                for(int j = i + 1; j < nums.length; j++){
+                    if(nums[i] == nums[j]){
+                        return true;
+                    }
+                }
+            }
         }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7,8,9,9};
+        System.out.println(containsNearbyDuplicate(nums,3));
 
     }
 }
