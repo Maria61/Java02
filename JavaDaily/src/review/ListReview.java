703edc5840103f1a2a2db6d6fa8b14e706cbe72c
Maria61 <mariaLi61@outlook.com>
2020-03-30 22:12:44 +0800

    Review

--- JavaDaily/src/review/ListReview.java
+++ JavaDaily/src/review/ListReview.java
@@ -5,15 +5,10 @@ package review;
  * @program JavaDaily
  * @date 2020/3/27 21:30
  */
+
 public class ListReview {
     public static void main(String[] args) {
         System.out.println();
-
-
-
-
-
-
-
+        Node node = new Node(1);
     }
 }
