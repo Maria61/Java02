443bc998464381db03a167608959d4443062d5aa
Maria61 <mariaLi61@outlook.com>
2020-04-10 23:23:24 +0800

    review

---
+++ JavaDaily/src/review/winter/Day06.java
@@ -0,0 +1,12 @@
+package review.winter;
+
+/**
+ * @author Maria
+ * @program JavaDaily
+ * @date 2020/4/9 16:28
+ */
+public class Day06 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
