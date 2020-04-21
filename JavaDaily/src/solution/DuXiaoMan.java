3cfce3a22283bf0b72613b93bc64faa4894bc345
Maria61 <mariaLi61@outlook.com>
2020-04-21 11:09:12 +0800

    DuXiaoMan

---
+++ JavaDaily/src/solution/DuXiaoMan.java
@@ -0,0 +1,20 @@
+package solution;
+
+/**
+ * @author Maria
+ * @program JavaDaily
+ * @date 2020/4/20 15:51
+ */
+public class DuXiaoMan {
+    /**
+     * 滑动窗口
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 传送门
+     */
+}
