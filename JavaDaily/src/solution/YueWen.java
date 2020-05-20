c44d8b3fcc2aaac6cb4e786475ec54d63279d171
Maria61 <mariaLi61@outlook.com>
2020-05-20 16:02:46 +0800

    被3整除+删除字符串中的QQ号

---
+++ JavaDaily/src/solution/YueWen.java
@@ -0,0 +1,16 @@
+package solution;
+
+/**
+ * @author Maria
+ * @program JavaDaily
+ * @date 2020/5/19 14:00
+ */
+public class YueWen {
+    /**
+     * 被3整除
+     */
+
+    /**
+     * 删除字符串中的QQ号
+     */
+}
