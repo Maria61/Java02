40b49dfd1325de2f9b33a9e272fed40ecb1331a3
Maria61 <mariaLi61@outlook.com>
2020-06-15 23:22:40 +0800

    sxf:将输入的字符串排序输出（从小到大）

---
+++ JavaDaily/src/solution/ShenXinFu2.java
@@ -0,0 +1,28 @@
+package solution;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author Maria
+ * @program JavaDaily
+ * @date 2020/6/15 18:45
+ */
+public class ShenXinFu2 {
+    //查看磁盘空间，文件属性，系统ip地址的linux命令
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] ss = new String[n];
+        for(int i = 0; i < n; i++){
+            ss[i] = sc.nextLine();
+        }
+        Arrays.sort(ss);
+        for(int i = 0; i < n; i++){
+            System.out.println(ss[i]);
+        }
+    }
+
+}
