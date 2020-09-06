b1e282da7018bb4a2129e3bce006fa3cfa003af5
Maria61 <mariaLi61@outlook.com>
2020-09-06 13:19:38 +0800

    同花顺

---
+++ JavaDaily/src/solution/TongHuaShun0905.java
@@ -0,0 +1,13 @@
+package solution;
+
+/**
+ * @author Maria
+ * @program JavaDaily
+ * @date 2020/9/5 8:32
+ */
+public class TongHuaShun0905 {
+    public static void main(String[] args) {
+        String s = "asd123456";
+        System.out.println(s.substring(3,4));
+    }
+}
