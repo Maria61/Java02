a9499465964114989e5ecf93cf5ac3e06f89ad13
Maria61 <mariaLi61@outlook.com>
2020-09-25 21:42:40 +0800

    笔试

---
+++ JavaDaily/src/solution/JiBiTe20200925.java
@@ -0,0 +1,12 @@
+package solution;
+
+/**
+ * @author Maria
+ * @program JavaDaily
+ * @date 2020/9/25 21:27
+ */
+public class JiBiTe20200925 {
+    public static void main(String[] args) {
+
+    }
+}
