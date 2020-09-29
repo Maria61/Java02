6bdecdcba58eda6c59e77d89b200a6b619e2b89e
Maria61 <mariaLi61@outlook.com>
2020-09-30 00:12:50 +0800

    两线程交替打印奇偶数问题

--- JavaDaily/src/review/TwoThreadPrint.java
+++ JavaDaily/src/review/TwoThreadPrint.java
@@ -6,5 +6,41 @@ package review;
  * @date 2020/9/29 23:37
  */
 public class TwoThreadPrint {
-
+    private static Object object = new Object();
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (object){
+                    for(int i = 1; i <= 10; i += 2){
+                        System.out.println(Thread.currentThread().getName()+":"+i);
+                        object.notify();//???
+                        try {
+                            object.wait();//???
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (object){
+                    for(int i = 2; i <= 10; i += 2){
+                        System.out.println(Thread.currentThread().getName()+":"+i);
+                        object.notify();//???
+                        try {
+                            object.wait();//???
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+    }
 }
